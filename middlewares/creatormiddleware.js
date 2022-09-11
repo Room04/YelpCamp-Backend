7a39b12b89b0c40b0b5bde5fbf39890d3cf57ec6
JacobJax <jackjax617@gmail.com>
2022-09-11 10:29:07 +0300

    :bug: fix camp update
    fix schema

--- middlewares/creatormiddleware.js
+++ middlewares/creatormiddleware.js
@@ -4,14 +4,13 @@ const Camp = require('../models/Camp')
 const creatorMiddleware = async (req, res, next) => {
    try {
       const camp = await Camp.findById(req.params.id, {"created_by.id": 1, _id: 0})
-
       // return error message if camp was not created by user
       if(camp.created_by.id.toString() != req.user._id) return res.status(401).json({error: "Access denied"}) 
       
       // continue if the campground requested was made by user     
       next()
    } catch (error) {
-      res.status(404).json({error: "Could not update. Try again later."})
+      res.status(404).json({error: "Could not update. Try again later.", msg: error.message})
    }
 }
 
