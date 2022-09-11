e67268e6b8185424cc7b4a436ded4b36e1bf5a89
JacobJax <jackjax617@gmail.com>
2022-09-11 09:46:18 +0300

    :recycle: adjust errors
    change error messages for camp update

--- middlewares/creatormiddleware.js
+++ middlewares/creatormiddleware.js
@@ -4,12 +4,17 @@ const Camp = require('../models/Camp')
 const creatorMiddleware = async (req, res, next) => {
    try {
       const camp = await Camp.findById(req.params.id, {"created_by.id": 1, _id: 0})
+
+      // return error message if campground does not exist
+      if(!camp) return res.status(404).json({error: "Campground not found"})
+
+      // return error message if camp was not created by user
       if(camp.created_by.id.toString() != req.user._id) return res.status(401).json({error: "Access denied"}) 
       
       // continue if the campground requested was made by user     
       next()
    } catch (error) {
-      res.status(404).json({error: "Campground not found"})
+      res.status(404).json({error: "An error occured.Try again later."})
    }
 }
 
